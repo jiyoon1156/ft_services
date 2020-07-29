48456ebe91759b4413008881adbb637b37b5b575
Jiyoon Hur <jhur@c2r5s5.42seoul.kr>
2020-07-29 16:52:14 +0900

    mkdir srcs

--- setup.sh
+++ setup.sh
@@ -20,31 +20,29 @@ IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed
 printf "Minikube IP: ${IP}"
 
 echo "Building images..."
-docker build -t nginx_img ./nginx/
-# docker build -t service_test ./srcs/test
-docker build -t ftps_img ./ftps --build-arg IP=${IP}
-docker build -t mysql_img ./mysql/ --build-arg IP=${IP}
-docker build -t wp_img ./wordpress --build-arg IP=${IP}
-docker build -t pma_img ./phpmyadmin --build-arg IP=${IP}
-docker build -t influxdb_img ./influxdb
-docker build -t telegraf_img ./telegraf
-docker build -t grafana_img ./grafana
+docker build -t nginx_img ./nginx
+docker build -t ftps_img ./srcs/ftps --build-arg IP=${IP}
+docker build -t mysql_img ./srcs/mysql --build-arg IP=${IP}
+docker build -t wp_img ./srcs/wordpress --build-arg IP=${IP}
+docker build -t pma_img ./srcs/phpmyadmin --build-arg IP=${IP}
+docker build -t influxdb_img ./srcs/influxdb
+docker build -t telegraf_img ./srcs/telegraf
+docker build -t grafana_img ./srcs/grafana
 
 #metallb
-
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/metallb.yaml
-kubectl apply -f config.yaml
+kubectl apply -f ./srcs/metalLB_config.yaml
 
 echo "Creating pods and services..."
-kubectl create -f nginx.yaml
-kubectl create -f mysql.yaml
-kubectl create -f phpmyadmin.yaml
-kubectl create -f wordpress.yaml
-kubectl create -f influxdb.yaml
-kubectl create -f telegraf.yaml
-kubectl create -f grafana.yaml
-kubectl create -f ftps.yaml
+kubectl create -f ./srcs/nginx.yaml
+kubectl create -f ./srcs/mysql.yaml
+kubectl create -f ./srcs/phpmyadmin.yaml
+kubectl create -f ./srcs/wordpress.yaml
+kubectl create -f ./srcs/influxdb.yaml
+kubectl create -f ./srcs/telegraf.yaml
+kubectl create -f ./srcs/grafana.yaml
+kubectl create -f ./srcs/ftps.yaml
 
 # echo "Opening the network in your browser"
 # open http://$IP
